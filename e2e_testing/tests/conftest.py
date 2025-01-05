d616628d58a5d4153eebb5244658f21ec3b81cfb
Shakhanina <Scah23frum@bk.ru>
2025-01-06 02:47:12 +0600

    headless set true

--- e2e_testing/tests/conftest.py
+++ e2e_testing/tests/conftest.py
@@ -4,7 +4,7 @@ from playwright.sync_api import sync_playwright
 
 @pytest.fixture(scope="session")
 def browser():
-    headless = os.getenv("HEADLESS", "false").lower() == "true"
+    headless = os.getenv("HEADLESS", "true").lower() == "true"
     browser_type = os.getenv("BROWSER", "chromium")
     with sync_playwright() as p:
         browser = getattr(p, browser_type).launch(headless=headless)
